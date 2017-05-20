209c21ebae73ebb1e0a5effe852bffdecbcf6e39
Silva Barros <asbarros@indra.es>
2017-05-20 17:16:26 -0300

    refactoring

--- host-map-web/src/app/historico-page/historico-page.component.ts
+++ host-map-web/src/app/historico-page/historico-page.component.ts
@@ -8,18 +8,19 @@ import {Localizacao} from "../model/localizacao.model";
 })
 export class HistoricoPageComponent implements OnInit {
 
-    historico: Array<Localizacao> = [
-        new Localizacao(-16.701, -49.2668, 'Google', 'www.google.com', '2017-05-20 16:54:00', 'United States'),
-        new Localizacao(-16.701, -49.2668, 'Google', 'www.google.com', '2017-05-20 16:54:00', 'United States'),
-        new Localizacao(-16.701, -49.2668, 'Google', 'www.google.com', '2017-05-20 16:54:00', 'United States'),
-        new Localizacao(-16.701, -49.2668, 'Google', 'www.google.com', '2017-05-20 16:54:00', 'United States'),
-        new Localizacao(-16.701, -49.2668, 'Google', 'www.google.com', '2017-05-20 16:54:00', 'United States'),
-        new Localizacao(-16.701, -49.2668, 'Google', 'www.google.com', '2017-05-20 16:54:00', 'United States')
-    ];
+    historico: Localizacao[];
 
     constructor() {
     }
 
     ngOnInit() {
+        this.historico = [
+            new Localizacao(-16.701, -49.2668, 'Google', '177.157.94.167', '2017-05-20 16:54:00', 'United States'),
+            new Localizacao(-16.701, -49.2668, 'Google', '177.157.94.167', '2017-05-20 16:54:00', 'United States'),
+            new Localizacao(-16.701, -49.2668, 'Google', '177.157.94.167', '2017-05-20 16:54:00', 'United States'),
+            new Localizacao(-16.701, -49.2668, 'Google', '177.157.94.167', '2017-05-20 16:54:00', 'United States'),
+            new Localizacao(-16.701, -49.2668, 'Google', '177.157.94.167', '2017-05-20 16:54:00', 'United States'),
+            new Localizacao(-16.701, -49.2668, 'Google', '177.157.94.167', '2017-05-20 16:54:00', 'United States')
+        ];
     }
 }
